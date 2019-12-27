9c01438f630d2df422a00cfaf1455aa6ed4e218a
Blake Irvin <blakeirvin@me.com>
2019-12-27 11:00:39 +0100

    Bugfix/circleci support (#4)
    * configure license acceptance
    * remove last Travis bits
    * declare `core/pcre` as a dependency
    
    Also standardize quoting of strings

--- functions/callbacks.sh
+++ functions/callbacks.sh
@@ -2,24 +2,27 @@
 # shellcheck disable=SC2034
 pkg_maintainer="Blake Irvin <blakeirvin@me.com>"
 # shellcheck disable=SC2034
-pkg_lib_dirs=(lib)
+pkg_lib_dirs=("lib")
 python_major_version="3.7"
 python_minor_version="0"
 # shellcheck disable=SC2034
 pkg_build_deps=(
-  bixu/cacher
-  core/curl
-  core/gcc
-  core/inetutils
-  core/jq-static
-  core/libffi
-  core/python/"${python_major_version}"."${python_minor_version}"
+  "bixu/cacher"
+  "core/pcre"
+  "core/curl"
+  "core/gcc"
+  "core/inetutils"
+  "core/jq-static"
+  "core/libffi"
+  "core/python/${python_major_version}.${python_minor_version}"
 )
 
 pkg_version() {
-  export LC_ALL=en_US LANG=en_US
+  export LC_ALL="en_US" LANG="en_US"
   # shellcheck disable=SC2154
-  pip search --disable-pip-version-check "${pkg_name}" | grep "^${pkg_name} " | cut -d\( -f2 | cut -d\) -f1
+  pip search --disable-pip-version-check "${pkg_name}" \
+    | grep "^${pkg_name} " \
+    | cut -d\( -f2 | cut -d\) -f1
 }
 
 do_before() {
@@ -44,7 +47,7 @@ do_prepare() {
   python -m venv "${pkg_prefix}"
   # shellcheck disable=SC1090
   source "${pkg_prefix}/bin/activate"
-  pip install --upgrade --quiet --no-cache-dir pip
+  pip install --upgrade --quiet --no-cache-dir "pip"
   return $?
 }
 
@@ -54,9 +57,12 @@ do_build() {
 }
 
 do_check() {
-  module_name=$(echo -n ${pkg_name} | cut -d- -f1) # python modules normally are imported by whatenver name comes before a `-`
+  module_name=$(echo -n ${pkg_name} | cut -d- -f1) # python modules normally
+                                                   # are imported by whatever
+                                                   # prefix comes before a `-`
   build_line "Attempting import of \`${module_name}\` Python module"
-  if python -c "import ${module_name}; print(${module_name}.__version__)" > /dev/null
+  if python -c "import ${module_name}; print(${module_name}.__version__)" \
+    > /dev/null
   then
     build_line "Import of  \`${module_name}\` Python module successful"
     return 0
@@ -73,12 +79,12 @@ do_install() {
 do_strip() {
   for module in $(pip freeze | grep -v $pkg_name==$pkg_version)
   do
-    pip uninstall --yes $module
+    pip uninstall --yes "${module}"
   done
-  rm -rf ${pkg_prefix}/lib/python3.6/site-packages/pip*
-  rm -rf ${pkg_prefix}/lib64/python3.6/site-packages/pip*
-  rm -rf ${pkg_prefix}/lib/python3.6/site-packages/setuptools*
-  rm -rf ${pkg_prefix}/lib64/python3.6/site-packages/setuptools*
+  rm -rf ${pkg_prefix}/lib/python${python_major_version}/site-packages/pip*
+  rm -rf ${pkg_prefix}/lib64/python${python_major_version}/site-packages/pip*
+  rm -rf ${pkg_prefix}/lib/python${python_major_version}/site-packages/setuptools*
+  rm -rf ${pkg_prefix}/lib64/python${python_major_version}/site-packages/setuptools*
   rm -rf ${pkg_prefix}/bin/pip*
   return $?
 }
